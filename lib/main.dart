a6b048fabe2816dcc646c10c5afc83d86dfeeed4
Jake Gordon <jakegordon53@gmail.com>
2025-03-05 20:47:15 +0000

    - integrated-firebase

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:satoshi_app/Login/login_view_model.dart';
 import 'login/login_view.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+WidgetsFlutterBinding.ensureInitialized();
+await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
+
   runApp(
     ChangeNotifierProvider(
       create: (context) => LoginViewModel(),
